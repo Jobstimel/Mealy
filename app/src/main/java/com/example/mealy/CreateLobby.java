499f2f0290a03055836775f3ca9ca52bf37a3fbb
jobi199 <chris.jobst@arcor.de>
2021-02-17 11:10:30 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/mealy/CreateLobby.java
@@ -0,0 +1,14 @@
+package com.example.mealy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateLobby extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_lobby);
+    }
+}
---
+++ app/src/main/java/com/example/mealy/PlayAlone.java
@@ -0,0 +1,14 @@
+package com.example.mealy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayAlone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_alone);
+    }
+}
